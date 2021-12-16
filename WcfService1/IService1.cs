5c09fd561f7e5080e3cd819f3a282f2aa9da520c
laith <laith@DESKTOP-AFE7T16>
2021-12-16 14:04:34 -0600

    Add project files.

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string InsertUserData(getwatchdata userInfo);
+        [OperationContract]
+        string SelectAllUserData();
+        [OperationContract]
+        string SelectOxygenData();
+        [OperationContract]
+        string SelectHeartData();
+        [OperationContract]
+        string SelectBloodData();
+        [OperationContract]
+        string SelectStepsData();
+        [OperationContract]
+        string SelectStairsData();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class getwatchdata
+    {
+        string oxygen_saturation;
+        string heart_rate;
+        string blood_pressure;
+        string steps_walked;
+        string stairs_climbed;
+
+        [DataMember]
+        public string Oxygen_Saturation
+        {
+            get { return oxygen_saturation; }
+            set { oxygen_saturation = value; }
+        }
+        [DataMember]
+        public string Heart_Rate
+        {
+            get { return heart_rate; }
+            set { heart_rate = value; }
+        }
+        [DataMember]
+        public string Blood_Pressure
+        {
+            get { return blood_pressure; }
+            set { blood_pressure = value; }
+        }
+        [DataMember]
+        public string Steps_Walked
+        {
+            get { return steps_walked; }
+            set { steps_walked = value; }
+        }
+        [DataMember]
+        public string Stairs_Climbed
+        {
+            get { return stairs_climbed; }
+            set { stairs_climbed = value; }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string SelectAllUserData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from HealthData", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
+        }
+        public string SelectOxygenData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Oxygen_Saturation from HealthData", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
+        }
+        public string SelectHeartData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Heart_Rate from HealthData", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
+        }
+        public string SelectBloodData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Blood_Pressure from HealthData", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
+        }
+        public string SelectStepsData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Steps_Walked from HealthData", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
+        }
+        public string SelectStairsData()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Stairs_Climbed from HealthData", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
+        }
+        public string InsertUserData(getwatchdata userInfo)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-AFE7T16;Initial Catalog=AppleWatchData;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into HealthData(Oxygen_Saturation,Heart_Rate,Blood_Pressure,Steps_Walked,Stairs_Climbed) values(@Oxygen_Saturation,@Heart_Rate,@Blood_Pressure,@Steps_Walked,@Stairs_Climbed)", con);
+            cmd.Parameters.AddWithValue("@Oxygen_Saturation", userInfo.Oxygen_Saturation);
+            cmd.Parameters.AddWithValue("@Heart_Rate", userInfo.Heart_Rate);
+            cmd.Parameters.AddWithValue("@Blood_Pressure", userInfo.Blood_Pressure);
+            cmd.Parameters.AddWithValue("@Steps_Walked", userInfo.Steps_Walked);
+            cmd.Parameters.AddWithValue("@Stairs_Climbed", userInfo.Stairs_Climbed);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = userInfo.Oxygen_Saturation + " Details inserted successfully";
+            }
+            else
+            {
+                Message = userInfo.Oxygen_Saturation + " Details not inserted successfully";
+            }
+            con.Close();
+            return Message;
+        }
+    }
+}
